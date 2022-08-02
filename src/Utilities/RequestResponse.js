94f811812baaf18eb8e0e730c258b6726366d2e1
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 04:27:49 +0500

    Update RequestResponse.js

--- src/Utilities/RequestResponse.js
+++ src/Utilities/RequestResponse.js
@@ -2,9 +2,6 @@ const axios = require('axios').default;
 const axiosInstance = axios.create({
     baseURL: "https://todolistdemobackend.herokuapp.com/api",
     withCredentials: true,
-    headers: {
-         "Content-type": "application/json",
-    },
 });
 
 
