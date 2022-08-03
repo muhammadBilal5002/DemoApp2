f60c3becaf8c07c84cdb9882baa5fa77fba667f9
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 16:48:16 +0500

    Update RequestResponse.js

--- src/Utilities/RequestResponse.js
+++ src/Utilities/RequestResponse.js
@@ -3,8 +3,7 @@ const axiosInstance = axios.create({
     baseURL: "https://todolistdemobackend.herokuapp.com/api",
     withCredentials: true,
     headers: {
-        //"Content-type": "application/json",
-        Cookie: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTliMWUyZDNmM2E2NDA0ZGQ3Y2M0MyIsImlhdCI6MTY1OTUyNjMyNSwiZXhwIjoxNjU5NjEyNzI1fQ._6RRc19jcOjV_wI71pGpx4M9bEzddUeuroSr8_eiHn8"
+        "Content-type": "application/json",
     },
 });
 
