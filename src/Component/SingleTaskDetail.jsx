60e37f2d95cc8714a1237acea231c31bad612610
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 18:09:24 +0500

    Update SingleTaskDetail.jsx

--- src/Component/SingleTaskDetail.jsx
+++ src/Component/SingleTaskDetail.jsx
@@ -11,7 +11,7 @@ return(
         <span id="one-task-description"><span className="bold">Description: </span>{props.value.description}</span>
         <span id="one-task-priority"><span className="bold">Priority: </span>{props.value.priority}</span>
         <span id="one-task-status"><span className="bold">Status: </span>{props.value.status}</span>
-        <span id="one-task-date"><span className="bold">Date: </span>{props.value.deadline}</span>
+        <span id="one-task-date"><span className="bold">Date: </span>{new Date(props.value.deadline).toString()}</span>
         <span id="one-task-remarks"><span className="bold">Remarks: </span>{props.value.remarks}</span>
     </div>
     </>
