f06553d62961d62da18aa59b92432383928a42c0
muhammadBilal5002 <57361001+muhammadBilal5002@users.noreply.github.com>
2022-08-03 06:22:38 +0500

    Update RequestResponse.js

--- src/Utilities/RequestResponse.js
+++ src/Utilities/RequestResponse.js
@@ -1,8 +1,9 @@
 const axios = require('axios').default;
 const axiosInstance = axios.create({
     baseURL: "https://todolistdemobackend.herokuapp.com/api",
+    crossDomain: true,
     withCredentials: true,
 });
 
-axiosInstance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+//axiosInstance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 export {axiosInstance}
